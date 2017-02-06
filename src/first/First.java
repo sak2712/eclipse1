ed77628ff99e1d6f74099cf4e661d5a72a265635
labcare_lko <labcare_lko@sakshi>
2017-02-06 14:07:46 +0530

    this is first class.

---
+++ src/first/First.java
@@ -0,0 +1,12 @@
+package first;
+
+public class First {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    System.out.println("First try with Git");
+    System.out.println("hello");
+    System.out.println("hi");
+	}
+
+}
